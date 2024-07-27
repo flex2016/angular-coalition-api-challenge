06bd65130384d02f804efe8affd9057304e14581
taras <reloded2004@hotmail.com>
2024-07-27 12:16:07 -0400

    updated to use temp dropdown for history

---
+++ src/app/components/diagnosis-chart/diagnosis-chart.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diagnosis-chart',
+  templateUrl: './diagnosis-chart.component.html',
+  styleUrl: './diagnosis-chart.component.scss',
+})
+export class DiagnosisChartComponent {}
--- src/app/components/diagnosis-history/diagnosis-history.component.ts
+++ src/app/components/diagnosis-history/diagnosis-history.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-diagnosis-history',
   templateUrl: './diagnosis-history.component.html',
-  styleUrl: './diagnosis-history.component.scss',
+  styleUrls: ['./diagnosis-history.component.scss'],
 })
-export class DiagnosisHistoryComponent {
+export class DiagnosisHistoryComponent implements OnInit {
   patient: any;
+  selectedMonth: any;
 
   constructor(private sharedService: SharedService) {}
 
   ngOnInit() {
     this.sharedService.selectedPatient.subscribe((patient) => {
       this.patient = patient;
+      if (patient) {
+        this.selectedMonth = patient.diagnosis_history[0];
+      }
     });
+  }
+
+  onMonthChange(event: Event) {
+    const selectedMonthName = (event.target as HTMLSelectElement).value;
+    this.selectedMonth = this.patient.diagnosis_history.find(
+      (month: any) => month.month === selectedMonthName
+    );
   }
 }
